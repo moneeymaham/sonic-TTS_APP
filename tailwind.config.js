186930f236d9ddbdb6df7d9e5f26a77f07741404
blockhead3100 <124408952+blockhead3100@users.noreply.github.com>
2025-08-23 05:19:27 +0000

    ok

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./config.js",
+    "./popup.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
